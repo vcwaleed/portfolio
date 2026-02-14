5a1d9d2d6bc5282f5052fd1d7d546714c5301628
Talha Waleed <talhawaleed460@gmail.com>
2026-02-14 19:38:44 +0200

    fix build

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
-import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    prerender({
-      staticDir: 'dist',
-      routes: ['/', '/about', '/projects', '/contact'],
-    }),
   ],
   resolve: {
     alias: {
